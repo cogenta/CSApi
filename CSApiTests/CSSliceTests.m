2d03044f7471bcba43f894021d7b1a57e62c0761
Will Harris <will@greatlibrary.net>
2013-08-13 18:11:44 +0100

    Test slice product list and search.

--- CSApiTests/CSSliceTests.m
+++ CSApiTests/CSSliceTests.m
@@ -25,6 +25,8 @@
 
 @property (strong) YBHALResource *productsResource;
 
+@property (strong) YBHALResource *productSearchResource;
+
 @end
 
 @implementation CSSliceTests
@@ -53,27 +55,27 @@
     
     //
 
-    productResource = [self resourceForFixture:@"product.json"];
+    productResource = [self resourceForFixture:@"slice_product_59t1CW.json"];
     STAssertNotNil(productResource, nil);
     
-    NSString *productsHref = [resource linkForRelation:@"/rels/products"].href;
-    STAssertNotNil(productsHref, nil);
-    NSString *productHref = [productResource linkForRelation:@"self"].href;
-    STAssertNotNil(productHref, nil);
-    NSMutableDictionary *productsDict = [NSMutableDictionary dictionary];
-    productsDict[@"_links"] = [NSMutableDictionary dictionary];
-    productsDict[@"_links"][@"self"] = [NSMutableDictionary dictionary];
-    productsDict[@"_links"][@"self"][@"href"] = productsHref;
-    productsDict[@"_links"][@"item"] = [NSMutableDictionary dictionary];
-    productsDict[@"_links"][@"item"][@"href"] = productHref;
-    productsDict[@"count"] = @1;
-    productsResource = [self resourceForJson:productsDict];
+    productsResource = [self resourceForFixture:@"slice_products.json"];
     STAssertNotNil(productsResource, nil);
     
     [requester addGetResponse:productResource
                        forURL:[productResource linkForRelation:@"self"].URL];
     [requester addGetResponse:productsResource
                        forURL:[productsResource linkForRelation:@"self"].URL];
+    
+    //
+    
+    self.productSearchResource = [self resourceForFixture:@"slice_product_search.json"];
+    NSURL *searchURL = [self.productSearchResource linkForRelation:@"self"].URL;
+    [requester addGetResponse:self.productSearchResource
+                       forURL:searchURL];
+    
+    YBHALResource *searchProductResource = [self resourceForFixture:@"slice_product_5WY4WK.json"];
+    [requester addGetResponse:searchProductResource
+                       forURL:[searchProductResource linkForRelation:@"self"].URL];
 }
 
 - (void)testGetProducts
@@ -97,7 +99,7 @@
     id<CSProductList> list = page.productList;
     STAssertNotNil(list, nil);
     
-    STAssertEqualObjects(@(list.count), @1, nil);
+    STAssertEqualObjects(@(list.count), @260235, nil);
     
     __block id<CSProduct> product = nil;
     error = [NSError errorWithDomain:@"not called" code:0 userInfo:nil];
@@ -115,6 +117,71 @@
     STAssertNotNil(product, nil);
     
     STAssertEqualObjects(product.name, self.productResource[@"name"], nil);
+}
+
+- (void)testProductSearch
+{
+    __block NSError *error = [NSError errorWithDomain:@"not called"
+                                                 code:0
+                                             userInfo:nil];
+    __block id<CSProductListPage> page = nil;
+    CALL_AND_WAIT(^(void (^done)()) {
+        [slice getProductsWithQuery:@"Apple iPod classic 160GB"
+                              callback:^(id<CSProductListPage> aPage,
+                                         NSError *anError)
+         {
+             page = aPage;
+             error = anError;
+             done();
+         }];
+    });
+    
+    STAssertNil(error, @"%@", error);
+    STAssertNotNil(page, nil);
+    
+    id<CSProductList> list = page.productList;
+    STAssertNotNil(list, nil);
+    
+    STAssertEqualObjects(@(list.count), @5, nil);
+    
+    __block id<CSProduct> product = nil;
+    error = [NSError errorWithDomain:@"not called" code:0 userInfo:nil];
+    CALL_AND_WAIT(^(void (^done)()) {
+        [list getProductAtIndex:0 callback:^(id<CSProduct> aProduct,
+                                             NSError *anError) {
+            product = aProduct;
+            error = anError;
+            done();
+        }];
+    });
+    
+    STAssertNil(error, @"%@", error);
+    STAssertNotNil(product, nil);
+    
+    STAssertEqualObjects(product.name,
+                         @"Apple iPod classic Silver - 160GB",
+                         nil);
+}
+
+- (void)testCancelProductSearch
+{
+    __block NSError *error = [NSError errorWithDomain:@"not called"
+                                                 code:0
+                                             userInfo:nil];
+    __block id<CSProductListPage> page = nil;
+    CALL_AND_WAIT(^(void (^done)()) {
+        [[slice getProductsWithQuery:@"Apple iPod classic 160GB"
+                            callback:^(id<CSProductListPage> aPage,
+                                       NSError *anError)
+          {
+              page = aPage;
+              error = anError;
+              done();
+          }] cancel];
+    });
+    
+    STAssertNotNil(error, nil);
+    STAssertNil(page, @"%@", page);
 }
 
 @end
